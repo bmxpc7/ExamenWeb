7772a91e041f50650fb249f6293dcec7d6f9a3b3
bmxpc7 <laser.hugo@hotmail.com>
2019-05-29 12:18:50 -0500

    Se completo la pagina

--- assets/js/Validaciones.js
+++ assets/js/Validaciones.js
@@ -1,20 +1,31 @@
 function validar() {
 	if ($('#txtNombre').val().length == 0) {
-		alert("Llene Nombre");
+		$('#txtNombre').css({ 'backgroundColor': 'red' });
 	}
 	if ($('#txtEmail').val().length == 0){
-		alert("Llene Email");
+		$('#txtEmail').css({ 'backgroundColor': 'red' });
 	}
 	if ($('#txtTelefono').val().length == 0) {
-		alert("Llene Telefono");
+		$('#txtTelefono').css({ 'backgroundColor': 'red' });
 	}
 }
 $("#btnRegresar").click(function() {
-	consumir();
+	$("#txtEmail").val("");
+	$("#txtNombre").val("");
+	$("#txtTelefono").val("");
+	$("#chck1").prop('checked',false);
+	$("#chck2").prop('checked',false);
+	$("#chck3").prop('checked',false);
+	$("#chck4").prop('checked',false);
+	$('#txtNombre').css({ 'backgroundColor': 'transparent' });
+	$('#txtEmail').css({ 'backgroundColor': 'transparent' });
+	$('#txtTelefono').css({ 'backgroundColor': 'transparent' });
+
 });
 
 $("#btnGuardar").click(function() {
 	validar();
+	ValCorreo();
 });
 
 function consumir(){
@@ -43,46 +54,64 @@ function consumir(){
 }
 
 function soloLetras(e){
-       key = e.keyCode || e.which;
-       tecla = String.fromCharCode(key).toLowerCase();
-       letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
-       especiales = "8-37-39-46";
+	key = e.keyCode || e.which;
+	tecla = String.fromCharCode(key).toLowerCase();
+	letras = " áéíóúabcdefghijklmnñopqrstuvwxyzÁÉÍÓÚABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+	especiales = "8-37-39-46";
 
-       tecla_especial = false
-       for(var i in especiales){
-            if(key == especiales[i]){
-                tecla_especial = true;
-                break;
-            }
-        }
+	tecla_especial = false
+	for(var i in especiales){
+		if(key == especiales[i]){
+			tecla_especial = true;
+			break;
+		}
+	}
 
-        if(letras.indexOf(tecla)==-1 && !tecla_especial){
-            return false;
-        }
-    }
+	if(letras.indexOf(tecla)==-1 && !tecla_especial){
+		return false;
+	}
+}
 
 function soloNumeros(e){
-       key = e.keyCode || e.which;
-       tecla = String.fromCharCode(key).toLowerCase();
-       numeros = "1234567890";
-       especiales = "8-37-39-46";
+	key = e.keyCode || e.which;
+	tecla = String.fromCharCode(key).toLowerCase();
+	numeros = "1234567890";
+	especiales = "8-37-39-46";
 
-       tecla_especial = false
-       for(var i in especiales){
-            if(key == especiales[i]){
-                tecla_especial = true;
-                break;
-            }
-        }
-
-        if(numeros.indexOf(tecla)==-1 && !tecla_especial){
-            return false;
-        }
-        if ($('#txtTelefono').val().length == 10) {
-		alert("Llene Telefono");
+	tecla_especial = false
+	for(var i in especiales){
+		if(key == especiales[i]){
+			tecla_especial = true;
+			break;
+		}
 	}
-    }
 
+	if(numeros.indexOf(tecla)==-1 && !tecla_especial){
+		return false;
+	}
+	if ($('#txtTelefono').val().length == 10) {
+		alert("Exedio el numero permitido");
+	}
+}
+
+function ValCorreo(e){
+	key = e.keyCode || e.which;
+	tecla = String.fromCharCode(key).toLowerCase();
+	letras = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ@.";
+	especiales = "8-37-39-46";
+
+	tecla_especial = false
+	for(var i in especiales){
+		if(key == especiales[i]){
+			tecla_especial = true;
+			break;
+		}
+	}
+
+	if(letras.indexOf(tecla)==-1 && !tecla_especial){
+		return false;
+	}
+}
 $( document ).ready(function() {
 	consumir();
 });
